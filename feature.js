db725496a3bfc4b74863b7715b166bf4f1566df6
eitanschreiber123 <eitanschreiber97@gmail.com>
2017-09-19 13:54:32 -0400

    Delete feature.js

--- feature.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'),
-  Schema = mongoose.Schema,
-  ObjectId = Schema.ObjectId;
-
-const featuresSchema = new Schema({
-   features: String
-});
-
-module.exports = mongoose.model('feature', featuresSchema);
