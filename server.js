d20df086d1b317c1f4c0e24cc4b07bdf0996d10b
eitanschreiber123 <eitanschreiber97@gmail.com>
2017-09-19 13:55:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let cors = require('cors');
 let Job = require('./Jobs');
-let feature = require('./feature');
 // REST
 mongoose.connect('mongodb://eitan:mlab1234@ds133084.mlab.com:33084/mean');
 
@@ -31,18 +30,6 @@ app.get("/jobs", (req,res) => {
   });
 });
 
-app.get("/new-feature", (req,res) => {
-  // retrieve the jobs from the database
-
-  feature.find((err, features) => {
-    if (err) {
-      return res.json({error: err.message, success: false});
-    }
-
-    res.json({error: false, success: true, features});
-  });
-});
-
 app.post("/jobs", (req, res) => {
 console.log('job data', req.body)
   const jobData = req.body.job;
@@ -59,36 +46,12 @@ console.log('job data', req.body)
   });
 });
 
-app.post("/new-feature", (req, res) => {
-console.log('feature data', req.body)
-  const featuresData = req.body.feature;
-
-  // save the job in the database
-  const featuresEntry = new featue(featuresData);
-
-  featuresEntry.save((err) => {
-    if (err) {
-      return res.json({error: err.message, success: false});
-    }
-
-    res.json({success: true, featue: featuresEntry});
-  });
-});
-
 app.put("/jobs/:_id", (req, res) => {
   const updatedJob = req.body.job;
 
   // use the id param on the url to fetch the existing job from the database
 
   res.json({success: true, job});
-});
-
-app.put("/new-feature/:_id", (req, res) => {
-  const updatedJob = req.body.feature;
-
-  // use the id param on the url to fetch the existing job from the database
-
-  res.json({success: true, feature});
 });
 
 app.delete("/jobs" , (req, res) => {
